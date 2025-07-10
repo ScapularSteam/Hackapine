993435908c4ae53f4e925c9e3db4627ecc9bc14b
David <86719656+ScapularSteam@users.noreply.github.com>
2025-07-10 23:30:32 +0100

    Open connection with Pinecil

--- script_testing/pinecil_scanner.py
+++ script_testing/pinecil_scanner.py
@@ -5,7 +5,7 @@ import sys
 
 # Scan all BLE devices and append devices with pinecil service UUID to found devices list
 found_devices = []
-async def main(found_devices):
+async def scan_device(found_devices):
 
     pinecil_service_UUID = "['9eae1000-9d0d-48c5-aa55-33e27f9bc533']"
     devices = await BleakScanner.discover(5.0, return_adv=True)
@@ -39,5 +39,22 @@ def select_device(found_devices):
             else:
                 return found_devices[(choice - 1)]
 
-asyncio.run(main(found_devices))
+# Get users Pinecil
+asyncio.run(scan_device(found_devices))
 pinecil = select_device(found_devices)
+
+# Open connection with Pinecil
+async def connect_device(pinecil):
+
+    async with BleakClient(pinecil) as client:
+        services = client.services
+
+        # List services for debugging purposes
+        print("Services:")
+        for service in services:
+            print(service)
+
+# Connect with pinecil
+asyncio.run(connect_device(pinecil))
+
+# TODO - connect with hackatime
